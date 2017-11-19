747139d4e41c0f5994677be6582b70e2f088743d
lorandKutvolgyi <lorand.kutvolgyi@gmail.com>
2017-11-19 10:04:57 +0100

    it worksgit add .!

--- bibletext.js
+++ bibletext.js
@@ -1,7 +1,103 @@
+var http = require('http');
+var database = require('./database');
+
+var books = ["GENESIS", "EXODUS", "LEVITICUS", "NUMBERS", "DEUTERONOMY", "JOSHUA", "JUDGES", "RUTH", "I_SAMUEL","II_SAMUEL", "I_KINGS",
+            "II_KINGS", "I_CHRONICLES", "II_CHRONICLES", "EZRA", "NEHEMIAH", "ESTHER", "JOB", "PSALMS", "PROVERBS", "ECCLESIASTES",
+            "SONG_OF_SOLOMON", "ISAIAH", "JEREMIAH", "LAMENTATIONS", "EZEKIEL", "DANIEL", "HOSEA", "JOEL", "AMOS", "OBADIAH", "JONAH",
+            "MICAH", "NAHUM", "HABAKKUK", "ZEPHANIAH","HAGGAI", "ZECHARIAH", "MALACHI", "MATTHEW", "MARK", "LUKE", "JOHN", "ACTS", "ROMANS",
+            "I_CORINTHIANS", "II_CORINTHIANS", "GALATIANS","EPHESIANS", "PHILIPPIANS", "COLOSSIANS", "I_THESSALONIANS", "II_THESSALONIANS",
+            "I_TIMOTHY", "II_TIMOTHY", "TITUS", "PHILEMON", "HEBREWS", "JAMES", "I_PETER", "II_PETER", "I_JOHN", "II_JOHN", "III_JOHN",
+            "JUDE", "REVELATION"];
+var books_abbrev = ["1Moz", "2Moz", "3Moz", "4Moz", "5Moz", "Jozs", "Bir", "Rut", "1Sam","2Sam", "1Kir",
+            "2Kir", "1Krón", "2Krón", "Ezsd", "Neh", "Eszt", "Jób", "Zsolt", "Péld", "Préd",
+            "En", "Ézs", "Jer", "Jsir", "Ez", "Dán", "Hós", "Joel", "Ám", "Abd", "Jón",
+            "Mik", "Náh", "Hab", "Zof","Hag", "Zak", "Mal", "Mat", "Mar", "Luk", "Jan", "ApCsel", "Rom",
+            "1Kor", "2Kor", "Gal","Ef", "Fil", "Kol", "1Tesz", "2Tesz",
+            "1Tim", "2Tim", "Tit", "Filem", "Zsid", "Jak", "1Pét", "2Pét", "1Jan", "2Jan", "3Ján",
+            "Jud", "Jel"];
+var translations = ['RUF', 'KG'];
+
+var i = 0;
+var j = 1;
+var m = 0;
+var translation = translations[m];
+var uri_fragment = books_abbrev[i]+j;
+var rows = [];
+
+var escape = function(text) {
+  var escaped = text.replace(';' , '\;').replace("'" , "\'").replace('"' , '\"');
+  return escaped;
+}
+
+var is_notexisting_chapter = function(data) {
+  console.log(data);
+  return JSON.parse(data).valasz.versek.length === 0;
+}
+
+var is_there_more_book = function(i) {
+  return i + 1 < books_abbrev.length;
+};
+
+var is_there_more_translation = function(m) {
+  return m + 1 < translations.length;
+};
+
+var process_data = function(data) {
+
+  var k = 0;
+  var num_of_verses = JSON.parse(data).valasz.versek.length;
+  while (k < num_of_verses){
+    var row = [books[i], j, k, escape(JSON.parse(data).valasz.versek[k].szoveg), translation];
+    rows.push(row);
+    k = k + 1;
+  }
+};
 
 var getBibleText = function(){
-  console.log('Getting the text of the Bible ...')
-  return 'this is the text';
+      http.get({
+        hostname: 'szentiras.hu',
+        port: 80,
+        path: '/api/idezet/'+ encodeURIComponent(uri_fragment) + '/' + translation,
+        agent: false
+      }, function(resp) {
+          var data='';
+
+          resp.on('data', function(chunk)  {
+            data += chunk;
+          });
+          resp.on('end', function() {
+            if(is_notexisting_chapter(data)){
+              if (is_there_more_book(i)){
+                i = i + 1;
+                j = 1;
+                uri_fragment = books_abbrev[i]+j;
+                setTimeout ( function() {
+                  getBibleText();
+                }, 0);
+              }else {
+                console.log(rows);
+                database.db.update_db(rows);
+                if (is_there_more_translation(m)) {
+                  translation = translations[++m];
+                  i = 0;
+                  j = 1;
+                  setTimeout ( function() {
+                    getBibleText();
+                  }, 60000);
+                }
+              }
+            } else {
+              process_data(data);
+              j = j + 1;
+              uri_fragment = books_abbrev[i]+j;
+              setTimeout ( function() {
+                getBibleText();
+              }, 0);
+            }
+          });
+    }).on("error", function(err)  {
+      console.log("Error: " + err.message);
+    });
 }
 
 module.exports.getBibleText = getBibleText;
--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var pg = require('pg');
 var postgresConnectionString = 'postgres://postgres:postgres@localhost:5432/postgres';
 var bibleConnectionString = 'postgres://postgres:postgres@localhost:5432/bible';
 
-var create_verses = 'CREATE TABLE verses(book varchar(50) not null, chapter integer not null, vers integer not null, content varchar(1000) not null, refs varchar(200), notes varchar(4000));';
+var create_verses = 'CREATE TABLE verses(book varchar not null, chapter integer not null, vers integer not null, content varchar not null, refs varchar, notes varchar,translation varchar);';
 var create_parts_of_chapters = 'CREATE TABLE parts_of_chapters(book varchar(50) not null, chapter integer not null, start_vers integer not null, end_vers integer not null, title varchar(100) not null, notes varchar(4000));';
 var create_chapters = 'CREATE TABLE chapters(book varchar(50) not null, chapter integer not null, title varchar(100) not null, notes varchar(4000));';
 var create_parts_of_books = 'CREATE TABLE parts_of_books(book varchar(50) not null, start_chapter integer not null, end_chapter integer not null, title varchar(100) not null, notes varchar(4000));';
@@ -11,34 +11,58 @@ var create_books = 'CREATE TABLE books(book varchar(50) not null, notes varchar(
 
 var db = (function () {
 
-  var connectToBible = function(callback) {
+  var create_tables = function(callback) {
     pg.connect(bibleConnectionString, function(err, bibleclient, done) {
         if (err) {
           console.log(err);
         } else {
-          bibleclient.query(create_verses+create_parts_of_chapters+create_chapters+create_parts_of_books+create_books, function(err, result) {//create_verses+create_parts_of_chapters+create_chapters+create_parts_of_books+create_books
-                console.log('tables are ready');
-                callback();
+          bibleclient.query(create_verses+create_parts_of_chapters+create_chapters+create_parts_of_books+create_books, function(err, result) {
+                if (err) {
+                  console.log('Error in creating tables' + err);
+                } else {
+                  console.log('tables are ready');
+                  callback();
+              }
           });
         }
     });
-  }
+  };
 
   return {
-    connect: function(overwrite, callback) {
+    create_db: function(overwrite, callback) {
       pg.connect(postgresConnectionString, function(err, client, done) {
           if (!err) {
             if (overwrite){
-               client.query('DROP DATABASE bible');
+               client.query('DROP DATABASE bible;');
             }
-            client.query('CREATE DATABASE bible', function(err, result) {
+            client.query('CREATE DATABASE bible;', function(err, result) {
               done();
-              connectToBible(callback);
+              create_tables(callback);
             });
           }
       });
     },
-  }
+
+    update_db: function(rows) {
+      pg.connect(bibleConnectionString, function(err, bibleclient, done) {
+          if (err) {
+            console.log('Error in connection: '+err);
+          } else {
+            for (var i = 0; i < rows.length; i++) {
+              var row = rows[i];
+              var query_string = 'INSERT INTO verses (book, chapter, vers, content, translation) VALUES(\'' + row[0] + '\', ' + row[1] + ', ' + row[2] + ', \'' + row[3] + '\', \'' + row[4] + '\')';
+
+              bibleclient.query(query_string, function(err, result) {
+                  if(err) {
+                    console.log('Error in query: '+err);
+                  }
+              });
+          }
+        }
+      });
+
+    }
+  };
 }());
 
 
